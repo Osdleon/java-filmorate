acfb937abe33186c3281cb6f02e101c584f7a36b
Dmitry Osipets <dmitryosipets@yandex.ru>
2023-06-10 21:04:51 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -1,15 +1,18 @@
 package ru.yandex.practicum.filmorate.storage.dao;
 
-import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 
 import java.util.Collection;
 
-@Component
-public class UserDbStorage implements UserStorage {
+public class UserDbStorage  implements UserStorage {
     private final JdbcTemplate jdbcTemplate;
 
+    public UserDbStorage(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+    @Override
     public void save(User user) {
 
     }
