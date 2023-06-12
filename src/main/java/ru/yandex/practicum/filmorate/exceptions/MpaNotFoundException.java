5d17e02648fc8c52ce53368dd586a75c0fb4945c
Dmitry Osipets <dmitryosipets@yandex.ru>
2023-06-12 22:10:49 +0300

    fix

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/MpaNotFoundException.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+import java.util.NoSuchElementException;
+
+public class MpaNotFoundException extends NoSuchElementException {
+    public MpaNotFoundException() {
+        super();
+    }
+}
